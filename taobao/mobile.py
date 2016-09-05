895adeb5f64f82e80d9bb6a42d62bba9658e931b
longzhen.wang <longzhen.wang@mi-me.com>
2016-09-05 20:33:19 +0800

    mobile

--- taobao/mobile.py
+++ taobao/mobile.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 """
-File Name : 'mobile'.py 
+File Name : 'mobile'.py
 Description:
-Author: 'wanglongzhen' 
+Author: 'wanglongzhen'
 Date: '2016/9/5' '9:23'
 """
 
@@ -36,7 +36,7 @@ class TransferAccounts(object):
         }
 
         #数据库连接
-        sql_pool = self.sql_connect()
+        # sql_pool = self.sql_connect()
 
         #不同类型
         self.keyword_call_type = {
@@ -57,10 +57,51 @@ class TransferAccounts(object):
         }
 
         #selenium 登录
-        self.homepage = 'http://www.10010.com'
-        login_url = 'http://www.10010.com/'
-        self.login_unicom(login_url, username, passwd)
+        self.homepage = 'http://www.10086.com'
+        login_url = 'https://login.10086.cn/login.html'
+        self.login_mobile(login_url, "13649258904", "728672")
 
+    def login_mobile(self, login_url, user, passwd):
+        """
+        登录移动商城
+        :param login_url:
+        :param username:
+        :param passwd:
+        :return:
+        """
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+
+        driver.delete_all_cookies()
+
+        self.logger = comm_log.CommLog(1)
+        self.logger.info(u'登录移动,用户名： ' + user + u'， 密码： ')
+
+        driver.get(login_url)
+        driver.find_element_by_id('p_name').clear()
+        driver.find_element_by_id('p_name').click()
+        driver.find_element_by_id('p_name').send_keys(user)
+        time.sleep(2)
+
+        driver.find_element_by_id('p_pwd').clear()
+        driver.find_element_by_id('p_pwd').send_keys(passwd)
+        time.sleep(2)
+
+        try:
+            element = driver.find_element_by_id('smspwdbord')
+
+            if element.is_displayed():
+                print("输入短信验证码:")
+                driver.find_element_by_id('getSMSPwd').click()
+                sms_id = raw_input("please input image code:")
+                driver.find_element_by_id('sms_pwd').send_keys(sms_id)
+            else:
+                print("不需要验证码")
+        except Exception, e:
+            pass
+        driver.find_element_by_id('submit_bt').click()
+
+        pass
 
 def main():
     transfer_accounts = TransferAccounts()
