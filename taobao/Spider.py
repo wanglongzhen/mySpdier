712e667706c3b445a54e3c0421ec9e2754060b68
longzhen.wang <longzhen.wang@mi-me.com>
2016-09-25 17:30:18 +0800

    demo

--- taobao/Spider.py
+++ taobao/Spider.py
@@ -5,3 +5,28 @@ Description:
 Author: 'wanglongzhen' 
 Date: '2016/9/24' '21:00'
 """
+
+from union import Union
+
+class MobileSpider(Union):
+    def __init__(self, username, passwd):
+        Union.__init__(self, username)
+        print 'MobileSpider init'
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+        Union.__exit__(self.exc_type, exc_val, exc_tb)
+
+    def login(self):
+        print 'MobileSpider login'
+        pass
+
+    def login_first_sms(self):
+        print 'MobileSpider login_first_sms'
+        pass
+
+    def login_sec_sms(self):
+        print 'MobileSpider login_sec_sms'
+        pass
+
